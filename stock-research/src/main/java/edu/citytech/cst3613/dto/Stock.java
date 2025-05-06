8ca27d8429afd3afea583f7039988f8401735134
Yu Chan <Yuzhench2004@gmail.com>
2025-05-06 17:28:21 -0400

    displaying the data

--- stock-research/src/main/java/edu/citytech/cst3613/dto/Stock.java
+++ stock-research/src/main/java/edu/citytech/cst3613/dto/Stock.java
@@ -4,4 +4,25 @@ import java.util.List;
 
 public class Stock {
     private List<String> data;
+    private int size;
+
+    public List<String> getData() {
+        return data;
+    }
+    public void setData(List<String> data) {
+        this.data = data;
+    }
+    public int getSize() {
+        return size;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
+    @Override
+    public String toString() {
+        return "Stock[]" +
+                "data=" + data +
+                ", size=" + size +
+                ']';
+    }
 }
