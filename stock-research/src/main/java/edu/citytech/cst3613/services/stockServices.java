23bbe7a7fee58ce49a3f3d853df5720fff5bae9b
Yu Chan <Yuzhench2004@gmail.com>
2025-05-06 17:13:30 -0400

    adding the stockServices

---
+++ stock-research/src/main/java/edu/citytech/cst3613/services/stockServices.java
@@ -0,0 +1,14 @@
+package edu.citytech.cst3613.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.citytech.cst3613.dto.Stock;
+
+public class stockServices {
+
+    List<Stock> getBillionDollars(){
+        var list = new ArrayList<Stock>();
+        return list;
+    }
+}
