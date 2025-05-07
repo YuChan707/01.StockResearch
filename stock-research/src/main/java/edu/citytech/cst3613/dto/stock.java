5b70f31b7525d0ea8b39dba1261bae7fb9234ed7
Yu Chan <Yuzhench2004@gmail.com>
2025-05-06 22:56:40 -0400

    adding the marketCapInMillions and divYield

--- stock-research/src/main/java/edu/citytech/cst3613/dto/stock.java
+++ stock-research/src/main/java/edu/citytech/cst3613/dto/stock.java
@@ -5,10 +5,13 @@ public class stock {
     public int rank = 0;
     public String symbol = "";
     public String companyName = "";
+    public float divYield = 0;
+    public float marketCapInMillions = 0;
 
     @Override
     public String toString(){
-        return "Stock [rank =" + rank + ", symbol" + symbol + ", companyName" + companyName + "]";
+        return "Stock [rank = " + rank + ", symbol = " + symbol + ", companyName = " + companyName 
+        + ", divYield = " + divYield + ", marketCapToBillions = " + marketCapInMillions + "]";
     }
     
 }
