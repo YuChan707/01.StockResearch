fcf8b5810eb70e51d5359d3e3841a80cb1d0b972
Yu Chan <Yuzhench2004@gmail.com>
2025-05-06 22:50:15 -0400

    adding the com.jbbwebsolutions.http and  com.google.code.gson

--- stock-research/src/main/java/module-info.java
+++ stock-research/src/main/java/module-info.java
@@ -3,7 +3,12 @@ module edu.citytech.cst3613 {
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.base;
+    requires java.net.http;
+    requires com.google.gson;
+    requires com.jbbwebsolutions.http.utility;
 
     opens edu.citytech.cst3613 to javafx.fxml;
+    opens edu.citytech.cst3613.services to com.jbbwebsolutions.http.utility;
+    opens edu.citytech.cst3613.dto to com.jbbwebsolutions.http.utility, com.google.gson;
     exports edu.citytech.cst3613;
 }
