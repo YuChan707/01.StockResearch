a44df7e4be2391cfd5869e864fabb11b2a1857ec
Yu Chan <Yuzhench2004@gmail.com>
2025-05-06 22:51:20 -0400

    take information form the server and display

---
+++ stock-research/src/main/java/edu/citytech/cst3613/services/stockService.java
@@ -0,0 +1,23 @@
+package edu.citytech.cst3613.services;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import static com.jbbwebsolutions.http.utility.JSONGet.*;
+import edu.citytech.cst3613.dto.stock;
+import edu.citytech.cst3613.dto.stockResult;
+
+public class stockService {
+    private static final String SURL = "http://localhost:8080/stocks/dividends";
+
+    List<stock> getStocks(){
+        var stockResults = submitGet(SURL, stockResult.class);
+        return stockResults.getData();
+    }
+
+    public static void main(String[] args) {
+        var services = new stockService();
+        services.getStocks().
+                forEach(System.out::println);
+    }
+}
